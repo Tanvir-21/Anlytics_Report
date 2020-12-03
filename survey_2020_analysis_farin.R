a1b38c97f74415a6552c4440a64acd4ec2d2d0e1
mashiat0808 <59837488+mashiat0808@users.noreply.github.com>
2020-12-03 23:00:43 +0600

    Add files via upload
    Added updated code and plots, separated bd, and worldwide plots. will start RMD soon.

--- survey_2020_analysis_farin.R
+++ survey_2020_analysis_farin.R
@@ -7,6 +7,7 @@ library(RColorBrewer)
 library(pacman)
 library(ggplot2)
 library(treemapify)
+library(ggpubr)
 
 #opening survery data file 
 survey_data <- read.csv(file.choose())
@@ -48,7 +49,7 @@ total_countries_plot <- highchart() %>%
   hc_colorAxis(stops = color_stops()) %>% 
   hc_title(text = "World Map") %>% 
   hc_subtitle(text = "Total respondent country in 2020") 
-total_countries_plot
+
 
 #plotting the treemap 
 
@@ -61,148 +62,258 @@ ggplot(top_countries, aes(area=Number, fill=factor(Country), label=Country))+
   geom_treemap_text(color="black", place="centre") + 
   scale_fill_manual(values=getPalette(colourCount)) + labs(title="TreeMap from top 20 Countries with most respondents")
 
-#dataframe of top countries
-top.responses <- survey_data[survey_data$Country=="United States" | survey_data$Country=="India" | survey_data$Country=="United Kingdom" | 
-                               survey_data$Country=="Germany" | survey_data$Country=="Canada" | survey_data$Country=="France " |
-                               survey_data$Country=="Brazil" | survey_data$Country=="Netherlands" | survey_data$Country=="Poland" |
-                               survey_data$Country=="Australia" | survey_data$Country=="Spain" | survey_data$Country=="Italy" |
-                               survey_data$Country=="Russian Federation" | survey_data$Country=="Sweden" | survey_data$Country=="Pakistan" |
-                               survey_data$Country=="Turkey" | survey_data$Country=="Israel" | survey_data$Country=="Switzerland" |
-                               survey_data$Country=="Bangladesh" | survey_data$Country=="Romania", ]
-  
-
-
-usa.responses<-survey_data[survey_data$Country=="United States",]
-india.responses<-survey_data[survey_data$Country=="India", ]
-difference.responses <-survey_data[survey_data$Country=="India"|survey_data$Country=="United States", ]
 
 #Barplot of responses according to country
-ggplot(top.responses,aes(y=factor(Country)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
-  labs(title= "Top responding countries",
-       y="Country",
-       x="Response",
-       color="blue") + theme_minimal()
+options(repr.plot.width=9, repr.plot.height=6) #setting height and width of the plot
+fillcolor1 <- "#FE8358"
+fillcolor2 <- "#F1FB92"
+line.length <- nrow(survey_data)
+
+percentage_topcountries <- survey_data %>% 
+  group_by(Country) %>% 
+  summarise(Counting=round(n()/line.length * 100, 2)) %>% 
+  arrange(desc(Counting)) %>% 
+  ungroup() %>% 
+  mutate(country = reorder(Country, Counting)) %>% 
+  head(20)
+percentage_topcountries_plot <- ggplot(percentage_topcountries, aes(x = country, y = Counting)) + 
+  geom_bar(stat = "identity", fill = fillcolor1)+
+  geom_text(aes(x = Country, y = 0.01, label = paste0("(", Counting, "% )", sep = "")),
+            hjust = -7, vjust = 1, size = 3, color = "black", fontface = "bold")+
+  scale_y_continuous()+
+  labs(x = "Country", y = "Percent", title = "Percent of respondent")+
+  coord_flip()
+percentage_topcountries_plot
+
 
 #developer by profession question response world plot
 
-ggplot(top.responses,aes(y=factor(MainBranch)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
+counting_developer_by_branch <- survey_data %>% 
+  filter(!is.na(MainBranch)) %>% 
+  group_by(MainBranch) %>% 
+  count()
+ggplot(survey_data,aes(y=factor(MainBranch)))+  
+  geom_bar(stat="count", width=0.7, fill=fillcolor1)+
   labs(title= "Professions of Respondents",
        y="Profession Criteria",
        x="Response") + theme_minimal()
 
-#developer by profession question response usa vs india plot
-
-profession <- difference.responses  %>%
-  group_by(MainBranch, Country)%>% 
-  summarise(Number=n())%>% 
-  arrange(desc=Number) %>%  select(MainBranch, Number, Country)
-
-
-ggplot(data=profession, aes(x=factor(MainBranch), y=Number, fill=Country)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()+ 
-  labs(title= "Comparison of Profession of respondents: India vs USA",
-       y="Profession",
-       x="Response") + theme_minimal()+ scale_fill_brewer(palette="Blues")
 
 
 #coding as hobby question world plot
-ggplot(top.responses,aes(y=factor(Hobbyist)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
+ggplot(survey_data,aes(y=factor(Hobbyist)))+  geom_bar(stat="count", width=0.7, fill="steelblue")+
   labs(title= "Coding as a hobby: Trait among respondents",
        y="Hobby",
        x="Response") + theme_minimal()
 
+#Finding employment Type 
+employment_type <- survey_data %>%
+  filter(!is.na(Employment)) %>% 
+  group_by(Employment) %>% 
+  count() 
 
-#coding as hobby question usa vs India plot
-hobby <- difference.responses  %>%
-  group_by(Hobbyist, Country)%>% 
-  summarise(Number=n())%>% 
-  arrange(desc=Number) %>%  select(Hobbyist, Number, Country)
+#Female to Male Ratio 
+##Female to Male Ration - Better Countries
+Female_male_ratio_plot <- survey_data %>% 
+  group_by(Country) %>% 
+  mutate(count = n()) %>% 
+  filter(count > 200) %>% 
+  filter(Gender %in% c('Man', 'Woman')) %>% 
+  group_by(Country, Gender) %>% 
+  count() %>% 
+  spread(Gender,n) %>% 
+  mutate(Female_to_male_ratio = Woman / Man) %>% 
+  arrange(desc(Female_to_male_ratio)) %>% 
+  head(50) %>% 
+  hchart('column', hcaes(x = 'Country', y = 'Female_to_male_ratio')) %>% 
+  hc_title(text = "Top 50 countries ordered with better female to male ratio") %>% 
+  hc_colors(c("steelblue", "darkgray")) %>% 
+  hc_xAxis(title = list(text = "Countries"))
+Female_male_ratio_plot
 
-
-ggplot(data=hobby, aes(x=factor(Hobbyist), y=Number, fill=Country)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()+ 
-  labs(title= "Comparison of coding as a hobby among respondents: India vs USA",
-       y="Hobby",
-       x="Response") +scale_fill_brewer(palette="Blues")
-
-
-#employment status world
-ggplot(top.responses,aes(y=factor(Employment)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
-  labs(title= "Employment status of Respondents",
-       y="Employment Criteria",
-       x="Response") + theme_minimal()
-      
-
-#usa vs india
-employment <- difference.responses  %>%
-  group_by(Employment, Country)%>% 
-  summarise(Number=n())%>% 
-  arrange(asc=Number) %>%  select(Employment, Number, Country)
-
-
-ggplot(data=employment, aes(x=factor(Employment), y=Number, fill=Country)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()+ 
-  labs(title= "Comparison of Employment of respondents: India vs USA",
-       y="Employment Status",
-       x="Response") + theme_minimal()+  scale_fill_brewer(palette="Blues")
-
-
-#formal education world
-ggplot(top.responses,aes(y=factor(EdLevel)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
-  labs(title= "Education Level of Respondents",
-       y="Education Level Criteria",
-       x="Response") + theme_minimal()
+#Male-female ratio among respondents 
+male_female_percentage <- survey_data %>% 
+  filter(!is.na(Gender)) %>% 
+  select(Gender) %>% 
+  mutate(Gender = str_split(Gender, pattern = ";")) %>% 
+  unnest(Gender) %>% 
+  group_by(Gender) %>% 
+  summarise(Count = n()) %>% 
+  arrange(desc(Count)) %>% 
+  mutate(percentage = round((Count/sum(Count))*100)) %>% 
+  arrange(desc(percentage))
 
 
-#usa vs india
-edulev <- difference.responses  %>%
-  group_by(EdLevel, Country)%>% 
-  summarise(Number=n())%>% 
-  arrange(asc=Number) %>%  select(EdLevel, Number, Country)
+
+# Which of the following best describes your main field of study (aka 'major')?
+TotalNoofRows <- nrow(survey_data)
+plotUndergradEducation <- function(survey_data, TotalNoofRows)
+{
+  survey_data %>% 
+    filter(!is.na(survey_data$UndergradMajor)) %>% 
+    select(UndergradMajor) %>% 
+    group_by(UndergradMajor) %>% 
+    summarise(Count = n()/TotalNoofRows) %>% 
+    arrange(desc(Count)) %>% 
+    ungroup() %>% 
+    mutate(UndergradMajor = reorder(UndergradMajor,Count)) %>% 
+    head(10) %>% 
+    ggplot(aes(x = UndergradMajor, y = Count))+
+    geom_bar(stat = 'identity', fill = fillcolor2)+
+    geom_text(aes(x = UndergradMajor,y = .01, label = paste0("( ",round(Count*100,2)," %)",sep="")),
+              hjust = -3, vjust = .5, size= 4, color = 'black', fontface = 'bold')+
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(
+      x = 'Undergrad Major',
+      y = 'Percentage',
+      title = 'Undergraduate Major Percentage'
+    )+
+    coord_flip()+
+    theme_bw()
+}
+plotUndergradEducation(survey_data, TotalNoofRows)
+
+# Developer Desire Next Year 
+## Platform Desire Next Year
+plotPlatformDesire <- function(survey_data,TotalNoofRows) {
+  survey_data %>%
+    filter(!is.na(PlatformDesireNextYear)) %>%
+    select(PlatformDesireNextYear) %>%
+    mutate(PlatformDesireNextYear = str_split(PlatformDesireNextYear, pattern = ";")) %>%
+    unnest(PlatformDesireNextYear) %>%
+    group_by(PlatformDesireNextYear) %>%
+    summarise(Count = n()) %>%
+    arrange(desc(Count)) %>%
+    ungroup() %>%
+    mutate(TotalCount =  sum(Count)) %>%
+    mutate(Count =  Count/TotalCount) %>%
+    mutate(PlatformDesireNextYear = reorder(PlatformDesireNextYear,Count)) %>%
+    head(10) %>%
+    
+    ggplot(aes(x = PlatformDesireNextYear,y = Count)) +
+    geom_bar(stat='identity',fill= fillcolor2) +
+    geom_text(aes(x = PlatformDesireNextYear,y = .01, label = paste0("( ",round(Count*100,2)," %)",sep="")),
+              hjust= -3, vjust=.5, size = 4, colour = 'black',
+              fontface = 'bold') +
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(x = 'PlatformDesireNextYear', 
+         y='Percentage', 
+         title = 'PlatformDesireNextYear and Percentage') +
+    coord_flip() +
+    theme_bw()
+}
+plotPlatformDesire(survey_data,TotalNoofRows)
+#Language worked with this 
+plotPlatformDesire <- function(survey_data,TotalNoofRows) {
+  survey_data %>%
+    filter(!is.na(LanguageWorkedWith)) %>%
+    select(LanguageWorkedWith) %>%
+    mutate(LanguageWorkedWith = str_split(LanguageWorkedWith, pattern = ";")) %>%
+    unnest(LanguageWorkedWith) %>%
+    group_by(LanguageWorkedWith) %>%
+    summarise(Count = n()) %>%
+    arrange(desc(Count)) %>%
+    ungroup() %>%
+    mutate(TotalCount =  sum(Count)) %>%
+    mutate(Count =  Count/TotalCount) %>%
+    mutate(LanguageWorkedWith = reorder(LanguageWorkedWith,Count)) %>%
+    head(10) %>%
+    
+    ggplot(aes(x = LanguageWorkedWith,y = Count)) +
+    geom_bar(stat='identity',fill= fillcolor2) +
+    geom_text(aes(x = LanguageWorkedWith,y = .01, label = paste0("( ",round(Count*100,2)," %)",sep="")),
+              hjust= -3, vjust=.5, size = 4, colour = 'black',
+              fontface = 'bold') +
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(x = 'Language Worked With', 
+         y='Percentage', 
+         title = 'Language worked with this year and Percentage') +
+    coord_flip() +
+    theme_bw()
+}
+
+plotPlatformDesire(survey_data,TotalNoofRows)
+
+#Salary analysis by developer type 
+
+##worldwide
+salary_by_developer_type_worldwide<- survey_data %>% 
+  filter(!is.na(DevType)) %>% 
+  select(DevType, ConvertedComp) %>% 
+  mutate(DevType = str_split(DevType, pattern = ";")) %>% 
+  unnest(DevType) %>% 
+  group_by(DevType) %>% 
+  summarise(median_salary = median(ConvertedComp, na.rm = TRUE)) %>% 
+  arrange(desc(median_salary)) %>% 
+  ungroup() %>% 
+  mutate(DevType = reorder(DevType, median_salary))
+
+salary_by_developer_type_worldwide_plot <- ggplot(data = salary_by_developer_type_worldwide,
+                                                  aes(x = DevType, y = median_salary))+
+  geom_bar(stat = 'identity', fill = fillcolor2)+
+  geom_text(aes(x = DevType, y = 1, label = paste0("( ",round(median_salary/1e3)," K)",sep="")),
+            hjust = -3, vjust = 0.5, size = 3, color = 'black', fontface = 'bold')+
+  labs(
+    x = 'DevType',
+    y = 'median_salary',
+    title = "Median Salary according to developer type Worldwide"
+  )+
+  coord_flip()+
+  theme_classic()
+
+survey_schema %>% 
+  filter(Column == "YearsCodePro") %>% 
+  select(QuestionText)
+
+## Salary Analysis Dev Types - Years of experiences 
+#with yearcodepro
+salary_by_dev_type <- survey_data %>% 
+  select(DevType, YearsCodePro, ConvertedComp) %>% 
+  mutate(DevType = str_split(DevType, pattern = ";")) %>% 
+  unnest(DevType) %>% 
+  group_by(DevType, YearsCodePro) %>% 
+  summarise(median_salary = median(ConvertedComp, na.rm = TRUE)) %>% 
+  arrange(desc(median_salary)) %>% 
+  ungroup() %>% 
+  mutate(YearsCodePro = as.character(YearsCodePro),
+         DevType = as.character(DevType)) %>% 
+  mutate(DevType_YearsCodePro = paste(DevType,YearsCodePro)) %>%
+  mutate(DevType_YearsCodePro =str_replace(DevType_YearsCodePro,"NA","")) %>%
+  mutate(DevType_YearsCodePro = reorder(DevType_YearsCodePro, median_salary)) %>% 
+  head(10)
 
 
-ggplot(data=edulev, aes(x=factor(EdLevel), y=Number, fill=Country)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()+ 
-  labs(title= "Comparison of Education Level of respondents: India vs USA",
-       y="Education Level Status",
-       x="Response") + theme_minimal()+  scale_fill_brewer(palette="Blues")
-
-
-#undergrad major world
-ggplot(top.responses,aes(y=factor(UndergradMajor)))+  geom_bar(stat="count", width=0.7, fill="LightBlue")+
-  labs(title= "Undergrad Major of Respondents",
-       y="Undergrad Major Criteria",
-       x="Response") + theme_minimal()
-#usa vs india
-major <- difference.responses  %>%
-  group_by(UndergradMajor, Country)%>% 
-  summarise(Number=n())%>% 
-  arrange(asc=Number) %>%  select(UndergradMajor, Number, Country)
-
-
-ggplot(data=major, aes(x=factor(UndergradMajor), y=Number, fill=Country)) +
-  geom_bar(stat="identity", position=position_dodge()) + coord_flip()+ 
-  labs(title= "Comparison of Undergrad Major of respondents: India vs USA",
-       y="Undergrad Major",
-       x="Response") + theme_minimal()+  scale_fill_brewer(palette="Greens")
-
-
-#importance of educational degree world, usa, india
-
-#age of people taking the survey world, usa, india
-
-#age first written code, world, usa, india
-
-#
-
-
-""
 ##########################################BANGLADESH ###############################################################
 
 
 #dataframe of bd
 bd <-survey_data[survey_data$Country=="Bangladesh",]
 
+#Salary analysis by developer type 
+salary_by_developer_type_Bangladesh <- survey_data %>% 
+  filter(Country == "Bangladesh") %>% 
+  filter(!is.na(DevType)) %>% 
+  select(DevType, ConvertedComp) %>% 
+  mutate(DevType = str_split(DevType, pattern = ";")) %>% 
+  unnest(DevType) %>% 
+  group_by(DevType) %>% 
+  summarise(median_salary = median(ConvertedComp, na.rm = TRUE)) %>% 
+  arrange(desc(median_salary)) %>% 
+  ungroup() %>% 
+  mutate(DevType = reorder(DevType, median_salary))
+
+salary_by_developer_type_Bangladesh_plot <- ggplot(data = salary_by_developer_type_Bangladesh,
+                                                   aes(x = DevType, y = median_salary))+
+  geom_bar(stat = 'identity', fill = fillcolor2)+
+  geom_text(aes(x = DevType, y = 1, label = paste0("( ",round(median_salary/1e3)," K)",sep="")),
+            hjust = -3, vjust = 0.5, size = 3, color = 'black', fontface = 'bold')+
+  labs(
+    x = 'DevType',
+    y = 'median_salary',
+    title = "Median Salary according to developer type in Bangladesh"
+  )+
+  coord_flip()+
+  theme_classic()
 
 #developer by profession 
 profession.bd <- bd %>% filter(!is.na(MainBranch))
